06952df52dcabd4d6972e29d2766aa075ef2c414
Khaii-Htoo <khaihtooag@gmail.com>
2025-08-02 17:40:52 +0530

    fixed suspense

--- app/(auth)/verify-email/page.tsx
+++ app/(auth)/verify-email/page.tsx
@@ -1,90 +1,10 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-} from "@/components/ui/card";
-import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSeparator,
-  InputOTPSlot,
-} from "@/components/ui/input-otp";
-import { authClient } from "@/lib/auth-client";
-import { Loader } from "lucide-react";
-import { redirect, useSearchParams } from "next/navigation";
-import React, { Suspense, useState, useTransition } from "react";
-import { toast } from "sonner";
+import React, { Suspense } from "react";
+import VerifyEmail from "./_components/verify-email";
 
 const page = () => {
-  const [otp, setOtp] = useState("");
-  const [verifyPending, startVerifyTransition] = useTransition();
-  const params = useSearchParams();
-  const email = params.get("email") as string;
-  const verifyEmail = async () => {
-    startVerifyTransition(async () => {
-      await authClient.signIn.emailOtp({
-        email,
-        otp,
-        fetchOptions: {
-          onSuccess: () => {
-            toast.success("Verify account successfully");
-            redirect("/");
-          },
-          onError: () => {
-            toast.error("failed email verify");
-          },
-        },
-      });
-    });
-  };
-
   return (
-    <Suspense fallback="loading...">
-      {" "}
-      <Card className=" min-w-md">
-        <CardHeader className=" text-center text-3xl">
-          Please check your email{" "}
-          <CardDescription className=" text-xl">
-            We have a sent verification code to your email addresss .Please open
-            the email and paste the code below
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col items-center">
-            <InputOTP
-              maxLength={6}
-              value={otp}
-              onChange={(value) => setOtp(value)}
-            >
-              <InputOTPGroup>
-                <InputOTPSlot index={0} />
-                <InputOTPSlot index={1} />
-                <InputOTPSlot index={2} />
-              </InputOTPGroup>
-              <InputOTPSeparator />
-              <InputOTPGroup>
-                <InputOTPSlot index={3} />
-                <InputOTPSlot index={4} />
-                <InputOTPSlot index={5} />
-              </InputOTPGroup>
-            </InputOTP>
-          </div>
-          <Button
-            className=" w-full my-5"
-            onClick={verifyEmail}
-            disabled={otp.length != 6 || verifyPending}
-          >
-            {verifyPending ? (
-              <Loader size={4} className=" animate-spin" />
-            ) : (
-              "Verify Account"
-            )}{" "}
-          </Button>
-        </CardContent>
-      </Card>
+    <Suspense fallback="Loading...">
+      <VerifyEmail />
     </Suspense>
   );
 };
