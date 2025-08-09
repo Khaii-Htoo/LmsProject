a73e24f19699b18fa1002790dee3a25c5833bf43
Khaii-Htoo <khaihtooag@gmail.com>
2025-08-09 16:30:09 +0530

    build

--- app/admin/course/[courseId]/page.tsx
+++ app/admin/course/[courseId]/page.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import CourseDetail from "./_components/course-detail";
 import Loading from "../../courses/[courseId]/edit/loading";
 
-const page = async ({ params }: { params: { courseId: string } }) => {
-  const { courseId } = params;
+interface PageProps {
+  params: { courseId: string };
+}
 
+const Page = async ({ params }: PageProps) => {
+  const { courseId } = params;
   const data = await adminGetCourseWithRelatedData(courseId);
-  console.log(data);
 
   return <div>{data ? <CourseDetail courseData={data} /> : <Loading />}</div>;
 };
 
-export default page;
+export default Page;
